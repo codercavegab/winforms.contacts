46bff32b18e8936e7b1baaf8e637d7c7bcf7fa22
codercavegab <60255224+codercavegab@users.noreply.github.com>
2022-02-09 12:46:23 -0300

    Adding a property from vs code web

--- WinFormsContacts/WinFormsContacts/Contact.cs
+++ WinFormsContacts/WinFormsContacts/Contact.cs
@@ -13,5 +13,6 @@ namespace WinFormsContacts
         public string LastName { get; set; }
         public string  Phone { get; set; }
         public string  Address { get; set; }
+        public string Alias  { get; set; }
     }
 }
